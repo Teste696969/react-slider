dec077e660b767f6ef41d9d3c28ed99fb06fbe9e
Teste696969 <recuperar20212009@gmail.com>
2025-11-13 07:16:00 -0300

    added fix

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -24,7 +24,7 @@ export function VideoPlayer({
   videos,
   initialVideoId,
   autoRandom = true,
-  autoLoop = true,
+  autoLoop = false,
   hiddenPrevious = false,
   hiddenNext = false,
   hiddenRandom = false,
